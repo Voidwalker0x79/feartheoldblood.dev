eb53df789d8720218b2bf0cbb4c1da6b2e3df2e1
Dillon Watts <dillon.watts@unity3d.com>
2021-12-03 13:32:04 -0600

    foo

--- scripts/spotify.sh
+++ scripts/spotify.sh
@@ -9,3 +9,7 @@ user_id=$(curl -s -X get "https://api.spotify.com/v1/me" "${headers[@]}" | jq -r
 get_top_tracks() {
     curl -s -X GET "https://api.spotify.com/v1/search" -G --data-urlencode "q=$1" --data-urlencode "type=track" --data-urlencode "limit=10" "${headers[@]}" | jq -r ".artists.items[0]?.id"
 }
+
+run() {
+    get_top_tracks()
+}
